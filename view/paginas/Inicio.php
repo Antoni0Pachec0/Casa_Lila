87c12d2f6897893a448581dc9d2f0b8f5111fe58
NoelMaldonado <maldonadonoel507@gmail.com>
2023-07-10 13:44:58 -0600

    Actualizacion de la vista de inicio

--- view/paginas/Inicio.php
+++ view/paginas/Inicio.php
@@ -7,14 +7,9 @@
     <link rel="stylesheet" href="http://localhost/casa_lila/html/css/inicio.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Home</title>
-
-    <style>
-        
-    </style>
 </head>
 <body>
     <!--Barra de navegacion superior-->
-
     <header class="header">
         <label for="btn_menu" class="menu"><i class='bx bx-menu' ></i></label>
 
@@ -25,10 +20,7 @@
 
         <div class="logo"></div>
 
-    <!--====================================================================================
-    *Condiciones para abrir las variables GET mediante la URL se condiciona si la 
-    *carpeta paginas es igual auna de las que tenemos se abra           #==:inicio.php==
-    =====================================================================================-->
+        <!--Condiciones para abrir las variables get mediante la URL-->
 
         <?php if(isset($_GET["paginas"])): ?>
 
@@ -38,7 +30,7 @@
             <a href="index.php?pagina=login">Inicia Sesion</a>
         <?php endif ?>
 
-<!--=================================================================-->
+        <!------------------------------------------------------------>
 
         <?php if($_GET["paginas"] == "culturales"): ?>
             <a href="index.php?pagina=culturales">Cursos culturales</a>
@@ -46,53 +38,56 @@
             <a href="index.php?pagina=culturales">Cursos culturales</a>
         <?php endif ?>
 
-<!--=================================================================-->
+        <!--=================================================================-->
 
-<?php if($_GET["paginas"] == "academicos"): ?>
-            <a href="index.php?pagina=academicos">Cursos academicos</a>
+        <?php if($_GET["paginas"] == "academicos"): ?>
+                    <a href="index.php?pagina=academicos">Cursos academicos</a>
+                <?php else: ?>
+                    <a href="index.php?pagina=academicos">Cursos academicos</a>
+                <?php endif ?>
+
+        <!--=================================================================-->
+
+        <?php if($_GET["paginas"] == "verano"): ?>
+                    <a href="index.php?pagina=verano">Cursos de verano</a>
+                <?php else: ?>
+                    <a href="index.php?pagina=verano">Cursos de verano</a>
+                <?php endif ?>
+
+        <!--=================================================================-->
+
+
         <?php else: ?>
-            <a href="index.php?pagina=academicos">Cursos academicos</a>
-        <?php endif ?>
-
-<!--=================================================================-->
-
-<?php if($_GET["paginas"] == "verano"): ?>
-            <a href="index.php?pagina=verano">Cursos de verano</a>
-        <?php else: ?>
-            <a href="index.php?pagina=verano">Cursos de verano</a>
-        <?php endif ?>
-
-<!--=================================================================-->
-
-
-        <?php else: ?>
-
-<!--=================================================================-->
 
         <nav class="navbar">
-            <a href="index.php?pagina=login">Inicia sesion</a>
+            <a href="index.php?pagina=login">Inicia sesión</a>
         </nav>
 
-        <a href="" class="perfil"><i class='bx bxs-user-circle' ></i></a>
+        <div class="opciones-perfil">
+            <label for="btn-perfil" class="perfil"><i class='bx bxs-user-circle' ></i></label>
+            <input type="checkbox" id="btn-perfil">
+            <div class="opciones">
+                <nav>
+                    <a href="">Perfil</a>
+                    <a href="">Mis cursos</a>
+                    <a href="">Cerrar sesión</a>
+                </nav>
+            </div>
+        </div>
     </header>
 
-<!--=================================================================-->
-
     <?php endif ?>
-    
-<!--=================================================================-->
-
 
     <input type="checkbox" id="btn_menu">
     <div class="container_menu">
         <div class="cont_menu">
             <nav>
-                <a href="#">Opcion 1</a>
-                <a href="#">Opcion 2</a>
-                <a href="#">Opcion 3</a>
-                <a href="#">Opcion 4</a>
-                <a href="#">Opcion 5</a>
-                <a href="#">Opcion 6</a>
+                <a href="#">Opcion</a>
+                <a href="#">Opcion</a>
+                <a href="#">Opcion</a>
+                <a href="#">Opcion</a>
+                <a href="#">Opcion</a>
+                <a href="#">Opcion</a>
             </nav>
             <label for="btn_menu" class="icon_equis"><i class='bx bx-x'></i></label>
         </div>
@@ -100,26 +95,44 @@
 
 <!--Abriremos esta seccion es para "seprar" la primera seccion la cual es la de informacio inicial-->
     <section class="inicio">
-<!--Esta etiqueta es para poner el titulo de la informacion-->
-        <label for="" id="titulo">Bienvenido</label>
-
-<!--Este div es para contener todo el parrafo y poder posicionarlo-->
-        <div>
-<!--Dentro de aqui va a ir el texto de la infromacion-->
-            <p id="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, molestiae iure omnis unde adipisci laboriosam harum nostrum repudiandae! Quos dolor quod praesentium vero fuga saepe numquam ea atque velit impedit.</p>
-        </div>
-
-<!--Aqui va a air la imagen promocional del lado dercho del section casi al mismo tamaño del mismo-->
-        <div class="gradient">
-            <img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/01lCIVJSjkjeeg5BYDawQi/ca2096a8eea91a495ebda8136e963487/ACV_KeyArt11__960x540.png" alt="">
+        <div class="fondo"></div>
+        <div class="info_s">
+            <h1>Bienvenido</h1>
+            <p>¿Se ha sorprendido alguna vez por todo lo que puede conseguir tan solo teniendo al profesor adecuado? Enhorabuena, acaba de llegar al sitio donde nunca va a parar de crecer y aprender.</p>
         </div>
     </section>
 
 <!--Este es el contenedor para mostrar todos los apartados de la pagina, la de curosos culturales academicos o culturales o de verano-->
-    
-    
-
     <main class="s-cursos">
+        <!--Contenedor de una de los apartados-->
+        <div class="cursos" id="culturales">
+            <!--La imagen promocional o demostrativa-->
+            <img src="https://www.diariomotor.com/imagenes/2023/03/f40-liberty-walk-641212ae4033f.jpg" alt="">
+            <!--El texto que va dentro del mismo-->
+            <h2>Cursos culturales</h2>
+            <p>¿Te interesa el arte? Encuentra actividades que ayuden a tu desarrollo artístico y diviertete aprendiendo.</p>
+            <a href="index.php?pagina=culturales">Saber más</a>
+        </div>
+
+        <!--Contenedor de una de los apartados-->
+        <div class="cursos" id="academicos">
+            <!--La imagen promocional o demostrativa-->
+            <img src="https://w0.peakpx.com/wallpaper/602/447/HD-wallpaper-ferrari-f40-back-view-aldix-racing-tuning-ferrari-f40-lm-supercars-white-f40-italian-cars-ferrari.jpg" alt="">
+            <!--El texto que va dentro del mismo-->
+            <h2>Cursos academicos</h2>
+            <p>Dí adiós a las dificultades y el aburrimiento, estudiar no debe ser tedioso, sino entretenido.</p>
+            <a href="index.php?pagina=academicos">Saber más</a>
+        </div>
+
+
+        <!--Contenedor de una de los apartados-->
+        <div class="cursos" id="verano">
+            <img src="https://www.diariomotor.com/imagenes/2023/03/f40-liberty-walk-641212ae4033f.jpg" alt="">
+            <h2>Cursos de verano</h2>
+            <p>¿Demasiado tiempo libre en vacaciones? Aprovéchalo y aprende algo nuevo.</p>
+            <a href="index.php?pagina=verano">Saber más</a>
+        </div>
+
         <div class="slider-frame">
             <ul>
                 <li><img src="https://images.pexels.com/photos/8134647/pexels-photo-8134647.jpeg" alt=""></li>
@@ -128,49 +141,17 @@
                 <li><img src="https://images.pexels.com/photos/8960867/pexels-photo-8960867.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""></li>
             </ul>
         </div>
-
-        <!--Contenedor de una de los apartados-->
-        <div class="cursos" id="culturales">
-            <!--La imagen promocional o demostrativa-->
-            <img src="https://www.diariomotor.com/imagenes/2023/03/f40-liberty-walk-641212ae4033f.jpg" alt="">
-            <!--El texto que va dentro del mismo-->
-            <a href="index.php?pagina=culturales">Cursos culturales</a>
-            <i class='bx bx-game'></i>
-        </div>
-
-        <!--Contenedor de una de los apartados-->
-        <div class="cursos" id="academicos">
-            <!--La imagen promocional o demostrativa-->
-            <img src="https://w0.peakpx.com/wallpaper/602/447/HD-wallpaper-ferrari-f40-back-view-aldix-racing-tuning-ferrari-f40-lm-supercars-white-f40-italian-cars-ferrari.jpg" alt="">
-            <!--El texto que va dentro del mismo-->
-            <a href="index.php?pagina=academicos">Cursos academicos</a>
-            <i class='bx bx-math'></i>
-        </div>
-
-
-        <!--Contenedor de una de los apartados-->
-        <div class="cursos" id="verano">
-            <!--La imagen promocional o demostrativa-->
-            <img src="https://s.aolcdn.com/os/ab/_cms/2023/04/27180101/Ferrari-F40-LB-Works-01.jpg" alt="">
-            <!--El texto que va dentro del mismo-->
-            <a href="index.php?pagina=verano">Cursos de verano</a>
-            <i class='bx bx-sun' ></i>
-        </div>
-
-
     </main>
 
     <article class="info">
-<!--Esta etiqueta es para poner el titulo de la informacion-->
-        <label for="">Informacion adicional</label>
+        <h2>Nuestra misión</h2>
 
 <!--Este div es para contener todo el parrafo y poder posicionarlo-->
         <div>
-<!--Dentro de aqui va a ir el texto de la infromacion-->
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, illo placeat. In iure veniam fugiat tempora aliquid cum accusamus at ducimus dolores nulla magnam assumenda culpa, voluptatum voluptatem voluptatibus cupiditate?</p>
+            <p>Es fácil hacer un gran trabajo si realmente se cree en lo que se hace. Por este motivo trabajamos cada dia para llegar a más personas. Más que un grupo de expertos, somos una familia con una visión en común. Nuestra pasión y conocimientos nos ayudan a marcar la diferencia.</p>
         </div>
 
-<!--Aqui va a air la imagen promocional del lado dercho del section casi al mismo tamaño del mismo-->
+<!--Aqui va a ir la imagen promocional del lado dercho del section casi al mismo tamaño del mismo-->
         <img src="https://2game.com/wp/wp-content/uploads/2021/12/AC-Valhalla-Unlock-DLC-for-all-players.jpg" alt="">
     </article>
 
@@ -202,5 +183,6 @@
             </ul>
         </div>
     </footer>
+
 </body>
 </html>
